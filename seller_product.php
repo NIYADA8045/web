92d262c3fcbef86b9254a9158400e9cb6d415015
@_D!vhed <55022692+prakan-suma@users.noreply.github.com>
2023-01-10 09:39:41 +0700

    Prakan : Update seller system

--- seller_product.php
+++ seller_product.php
@@ -5,17 +5,21 @@ sellerAuth();
 <!-- //main  -->
 <main class="p-5">
     <h4>สินค้าร้าน <?= user('shop_name') ?></h4>
+    <form action="seller_product_delete.php" method="POST">
     <table class="table m-auto">
         <thead>
-            <th>รหัสสินค้า</th>
-            <th>ชื่อ</th>
-            <th>รูปภาพ</th>
-            <th>ลายระเอียด</th>
-            <th>กว้าง</th>
-            <th>สูง</th>
-            <th>ยาว</th>
-            <th>น้ำหนัก</th>
-            <th>&nbsp;</th>
+            <tr>
+                <th><input name="id[]" type="checkbox" onclick="this" ></th>
+                <th>รหัสสินค้า</th>
+                <th>ชื่อ</th>
+                <th>รูปภาพ</th>
+                <th>ลายระเอียด</th>
+                <th>กว้าง</th>
+                <th>สูง</th>
+                <th>ยาว</th>
+                <th>น้ำหนัก</th>
+                <th>&nbsp;</th>
+               </tr>
         </thead>
 
         <tbody>
@@ -34,8 +38,8 @@ sellerAuth();
                     <td> <?= $p['box_long'] ?></td>
                     <td> <?= $p['box_weight'] ?></td>
                     <td>
-                        <a class="btn btn-block btn-primary">แก้ไขสินค้า</a>
-                        <a class="btn btn-block btn-danger">ลบ</a>
+                        <a class="btn btn-block btn-primary" href="seller_product_edit.php?pid=<?=$p['id']?>">แก้ไขสินค้า</a>
+                        <a class="btn btn-block btn-danger" href="seller_product_delete.php?pid=<?=$p['id']?>">ลบ</a>
                     </td>
                 </tr>
             <?php
@@ -44,9 +48,15 @@ sellerAuth();
 
         </tbody>
     </table>
-    <div class="col-3">
-        <a href="seller_product_add.php" class="btn block btn-primary">เพิ่มสินค้า</a>
-    </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn block btn-primary" type="submit">ลบสินค้า</button>
+            </div>
+            <div class="col">
+                <a href="seller_product_add.php" class="btn block btn-primary">เพิ่มสินค้า</a>
+            </div>
+        </div>
+    </form>
 </main>
 
 <?php
